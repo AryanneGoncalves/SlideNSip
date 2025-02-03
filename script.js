6f1b405fd645a73c94f0de5a0940b72f0de2940c
Aryanne Gonçalves <aryannegoncalves529@gmail.com>
2025-02-03 19:58:43 -0300

    primeira etapa

---
+++ script.js
@@ -0,0 +1,75 @@
+const imgSlider = document.querySelector('.img-slider');
+const imgFruits = document.querySelectorAll('.img-item.fruit');
+const infoSlider = document.querySelector('.info-slider');
+
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+let indexSlider = 0;
+let index = 0;
+let direction;
+
+nextBtn.addEventListener('click', () => {
+
+    indexSlider++;
+    imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+
+    index++;
+    if (index > imgFruits.length - 1) {
+        index = 0;
+    }
+
+    document.querySelector('.fruit.active').classList.remove('active');
+    imgFruits[index].classList.add('active'); 
+
+    direction = -1;
+    console.log(direction);
+
+    infoSlider.style.transform = 'translateY(-25%)';
+
+});
+
+prevBtn.addEventListener('click', () => {
+
+    indexSlider--;
+    imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+
+    index--;
+    if (index < 0) {
+        index = imgFruits.length - 1;
+    }
+    
+    document.querySelector('.fruit.active').classList.remove('active');
+    imgFruits[index].classList.add('active');
+
+    if (direction == 1){
+        infoSlider.appendChild(infoSlider.lastElementChild);
+    }
+
+    direction = 1;
+
+
+    infoSlider.style.transform = 'translateY(25%)';
+
+});
+
+infoSlider.addEventListener('transitionned', () => {
+
+    if (direction == -1){
+        infoSlider.appendChild(infoSlider.firstElementChild);
+    }
+    else if (direction ==1) {
+        infoSlider.prepend(infoSlider.lastElementChild);   
+    }
+
+    infoSlider.style.transition = 'none';
+    infoSlider.style.transform = 'translateY(0)';
+
+    setTimeout(() => {
+        infoSlider.style.transition = '.5s cubic-bezier(0.645, 0.045, 0.355, 1)';
+
+    });
+});
+
+
+
